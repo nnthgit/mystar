4c9da4fddb41bf7d711d42bfe455879b63010e4c
nnthgit <121067773+nnthgit@users.noreply.github.com>
2024-03-10 03:51:58 +0000

    etoile.py

--- etoile.py
+++ etoile.py
@@ -1,12 +1,12 @@
 import turtle
 
-turtle.bgcolor('red') // couleur du background
+turtle.bgcolor('red') # couleur du background
 
 star = turtle.Turtle()
-star.color("green") // couleur
-star.width(15)
+star.color("green") # couleur de l'étoile
+star.width(15) # Epaisseur du trait
 star.right(75)
-star.forward(250)  // longueur
+star.forward(250)  # longueur des segments
 
 for i in range(4):
     star.right(144)
